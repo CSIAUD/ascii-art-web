cb3f892c6d3b6a2c306bae5779499203ee26bc18
Cyprien <cyprien.siaud@ynov.com>
2021-03-01 09:54:28 +0100

    [ADD] affichage ASCII

---
+++ js/ascii-art.js
@@ -0,0 +1,72 @@
+const input=document.getElementById('input'); // Récupération de l'entrée de texte
+const output=document.getElementById('output'); // Récupération de la sortie du ascii-art
+const fichier0=document.getElementById('fichier0'); // Récupération du 1er fichier ascii
+const fichier1=document.getElementById('fichier1'); // Récupération du 2eme fichier ascii
+const fichier2=document.getElementById('fichier2'); // Récupération du 3eme fichier ascii
+
+const ascii=[JSON.parse(fichier0.innerText),JSON.parse(fichier1.innerText),JSON.parse(fichier2.innerText)];
+
+let chars=['!','\"','#','$','%','&','\'','(',')','*','+',',','-','.','/','0','1','2','3','4','5','6','7','8','9',':',';','<','=','>','?','@','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','[','\\',']','^','_','`','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','{','|','}','~','\n',' '];
+
+function afficherAscii(){
+    let text = ""
+    let lettres=[];
+    lettres.push([]);
+    let nbRt=0;
+
+    for(let val of input.value){
+        for(let char of chars){
+            if(val === char){
+                text += val
+                break
+            }
+        }
+    }
+    console.log(text)
+
+    for(let i=0; i<text.length; i++){
+        if(text[i]==='\n'){
+            lettres.push([]);
+            nbRt++;
+        }else{
+            lettres[nbRt].push(chars.indexOf(text[i]));
+        }
+    }
+
+    let choix = 0;
+
+    let txt="";
+    for(let l=0; l<=nbRt; l++){
+        txt+="<table cellspacing='0' cellpadding='0'><tr>";
+
+        let i=0;
+        let j=0;
+        let k=0;
+
+        while(i<8){
+            txt+= "<tr>";
+            while(j<lettres[l].length){
+                if(lettres[l][j]===95){
+                    txt+="<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>";
+                }else{
+                    while(k<ascii[choix][lettres[l][j]][i].length){
+                        if (ascii[choix][lettres[l][j]][i][k]===' '){
+                            txt+="<td>&nbsp;&nbsp;</td>";
+                        }else{
+                            txt+="<td>"+ascii[choix][lettres[l][j]][i][k]+"</td>";
+                        }
+                        k++;
+                    }
+                }
+                j++;
+                k=0;
+            }
+            txt+= "</tr>";
+            i++;
+            j=0;
+        }
+        txt+="</table>";
+    }
+
+    output.innerHTML=txt;
+}
