3d30c6946ed1a41517386d51d22b72c8d9189121
Cyprien <cyprien.siaud@ynov.com>
2021-03-01 10:04:37 +0100

    [ADD] gestion de la couleur

--- js/ascii-art.js
+++ js/ascii-art.js
@@ -1,18 +1,30 @@
 const input=document.getElementById('input'); // Récupération de l'entrée de texte
 const output=document.getElementById('output'); // Récupération de la sortie du ascii-art
+
+const color=document.getElementById('color'); // Récupération du choix de la couleur
+const police=document.getElementsByName('font'); // Récupération du choix de la police à utiliser
+
 const fichier0=document.getElementById('fichier0'); // Récupération du 1er fichier ascii
 const fichier1=document.getElementById('fichier1'); // Récupération du 2eme fichier ascii
 const fichier2=document.getElementById('fichier2'); // Récupération du 3eme fichier ascii
 
 const ascii=[JSON.parse(fichier0.innerText),JSON.parse(fichier1.innerText),JSON.parse(fichier2.innerText)];
 
-let chars=['!','\"','#','$','%','&','\'','(',')','*','+',',','-','.','/','0','1','2','3','4','5','6','7','8','9',':',';','<','=','>','?','@','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','[','\\',']','^','_','`','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','{','|','}','~','\n',' '];
+const chars=['!','\"','#','$','%','&','\'','(',')','*','+',',','-','.','/','0','1','2','3','4','5','6','7','8','9',':',';','<','=','>','?','@','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','[','\\',']','^','_','`','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','{','|','}','~','\n',' '];
+
+color.addEventListener('load', () => {
+    output.style.color = color.value
+})
+color.addEventListener('change', () => {
+    output.style.color = color.value
+})
 
 function afficherAscii(){
     let text = ""
     let lettres=[];
     lettres.push([]);
     let nbRt=0;
+    let font = 0;
 
     for(let val of input.value){
         for(let char of chars){
@@ -33,7 +45,6 @@ function afficherAscii(){
         }
     }
 
-    let choix = 0;
 
     let txt="";
     for(let l=0; l<=nbRt; l++){
@@ -49,11 +60,11 @@ function afficherAscii(){
                 if(lettres[l][j]===95){
                     txt+="<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>";
                 }else{
-                    while(k<ascii[choix][lettres[l][j]][i].length){
-                        if (ascii[choix][lettres[l][j]][i][k]===' '){
+                    while(k<ascii[font][lettres[l][j]][i].length){
+                        if (ascii[font][lettres[l][j]][i][k]===' '){
                             txt+="<td>&nbsp;&nbsp;</td>";
                         }else{
-                            txt+="<td>"+ascii[choix][lettres[l][j]][i][k]+"</td>";
+                            txt+="<td>"+ascii[font][lettres[l][j]][i][k]+"</td>";
                         }
                         k++;
                     }
@@ -69,4 +80,7 @@ function afficherAscii(){
     }
 
     output.innerHTML=txt;
+    output.style.color = color.value
 }
+
+afficherAscii()
